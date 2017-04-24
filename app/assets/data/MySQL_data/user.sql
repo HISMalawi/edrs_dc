360792d6141dc86364ba3c89c8f95b22a4a24818
cranberry <manzymkamba@gmail.com>
2017-04-24 12:32:49 +0200

    Mysql Dumping included in DC

---
+++ app/assets/data/MySQL_data/user.sql
@@ -0,0 +1,28 @@
+        DROP TABLE IF EXISTS `user`;
+        CREATE TABLE `user` (
+        `user_id` VARCHAR(225) NOT NULL,
+      `username` VARCHAR(255) DEFAULT NULL,
+      `first_name` VARCHAR(255) DEFAULT NULL,
+      `last_name` VARCHAR(255) DEFAULT NULL,
+      `password_hash` VARCHAR(255) DEFAULT NULL,
+      `last_password_date` datetime DEFAULT NULL,
+      `password_attempt` INT(11) DEFAULT NULL,
+      `login_attempt` INT(11) DEFAULT NULL,
+      `email` VARCHAR(255) DEFAULT NULL,
+      `active` tinyint(1) NOT NULL  DEFAULT '0',
+      `notify` tinyint(1) NOT NULL  DEFAULT '0',
+      `role` VARCHAR(255) DEFAULT NULL,
+      `district_code` VARCHAR(255) DEFAULT NULL,
+      `site_code` VARCHAR(255) DEFAULT NULL,
+      `preferred_keyboard` VARCHAR(255) DEFAULT NULL,
+      `creator` VARCHAR(255) DEFAULT NULL,
+      `plain_password` VARCHAR(255) DEFAULT NULL,
+      `un_or_block_reason` VARCHAR(255) DEFAULT NULL,
+      `_rev` VARCHAR(255) DEFAULT NULL,
+      `updated_at` datetime DEFAULT NULL,
+      `created_at` datetime DEFAULT NULL,
+      PRIMARY KEY (`user_id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+
+INSERT INTO user (user_id, username, first_name, last_name, password_hash, last_password_date, password_attempt, login_attempt, email, active, notify, role, district_code, site_code, preferred_keyboard, creator, plain_password, un_or_block_reason, _rev, updated_at, created_at) VALUES 
+('ca9785092fa94b0eca59f90e081e5cde', "admin","EDRS","Administrator","$2a$10$/VV1sdgjhwSPdvtoGaArO.YgOmgk4B1pRtvff2moz71EbJ61lugx6","2017-04-23 09:20:13",0,0,"admin@baobabhealth.org",true,0, "System Administrator",NULL, "HQ","abc","admin",NULL, NULL, "1-de79ca4089b858fb752b1767c031277e","2017-04-23 09:20:13","2017-04-23 09:20:13");
--- config/routes.rb
+++ config/routes.rb
@@ -249,6 +249,16 @@ Rails.application.routes.draw do
 
   get "/my_account" => "users#my_account"
 
+  get "/build_mysql" => "users#build_mysql"
+
+  get '/build_mysql_database' => 'users#build_mysql_database'
+
+  get 'create_mysql_database/:page_number/:records_per_page/:model_name/:table_name/:table_primary_key' => 'users#create_mysql_database'
+  
+  post '/database_load' => 'users#database_load'
+  
+  get 'database_load_progress/:table_name' => 'users#database_load_progress'
+
 ######################################################################################################################################################################################
 
 ################################ Routes for Reports ###################################################################################################################################
