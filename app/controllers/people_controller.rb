926e80a738d3ccfa2f6744f92f3500a7415a88b9
cranberry <manzymkamba@gmail.com>
2018-02-15 13:51:31 +0200

    District code in elastic search

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -501,7 +501,7 @@ class PeopleController < ApplicationController
               record["father_middle_name"] = (@person.father_middle_name rescue nil)
               record["father_first_name"] = (@person.father_first_name rescue nil)
               record["id"] = @person.id
-              record["district_code"] = SETTINGS['district_code']
+              record["district_code"] = @person.district_code
               SimpleElasticSearch.add(record)
           end
 
