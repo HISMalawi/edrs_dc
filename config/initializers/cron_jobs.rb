1eec2e9d7b2dea6113f2096627dc5c9b32bb6df1
cranberry <manzymkamba@gmail.com>
2018-02-27 09:22:41 +0200

    Initiate clon jobs

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -5,13 +5,13 @@ if SETTINGS['site_type'].to_s != "facility"
     AssignDen.perform_in(3)
 end
 if Rails.env == 'development'
-     #SyncData.perform_in(60)
+     SyncData.perform_in(60)
 else
   	 SyncData.perform_in(900)
 end
 
 if Rails.env == 'development'
-    #UpdateSyncStatus.perform_in(90)
+    UpdateSyncStatus.perform_in(90)
 else
   	UpdateSyncStatus.perform_in(1000)
 end
