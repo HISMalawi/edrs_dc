3031596c11d8e88b4c9936fe464e1e5eccc36f88
cranberry <manzymkamba@gmail.com>
2018-05-30 13:32:04 +0200

    Create users scrpts

--- bin/scripts/create_users.rb
+++ bin/scripts/create_users.rb
@@ -2,7 +2,7 @@ users  = {
 		"clerk" => {"role" => "Data Clerk",
 				"first_name" => "NRB",
 				"last_name" => "Data Clerk"
-		}
+		},
 		"admin" => {"role" => "System Administrator",
 				"first_name" => "NRB",
 				"last_name" => "System Administrator"
@@ -10,8 +10,8 @@ users  = {
 		"logistics" => {"role" => "Logistics Officer",
 				"first_name" => "NRB",
 				"last_name" => "Logistics officer"
-		}
-		,"adr" => {
+		},
+		"adr" => {
 				"role" => "ADR",
 				"first_name" => "NRB",
 				"last_name" => "ADR"
@@ -26,25 +26,39 @@ if SETTINGS['site_type'] == "facility"
 								email: "admin@baobabhealth.org")
 		puts "#{user.username} : p@ssw0rd"
 elsif SETTINGS['site_type'] == "dc"
-      surfix = SETTINGS['district_code']
+      surfix = SETTINGS['district_code'].downcase
       puts "Login credentials"
+
 	  users.keys.each do |username|
-		user = User.create(username: "#{username}#{surfix}", plain_password: "p@ssw0rd", last_password_date: Time.now,
-								password_attempt: 0, login_attempt: 0, first_name: users[username]["first_name"],
-								last_name: users[username]["last_name"], role: users[username]["role"],
-								email: "admin@baobabhealth.org")
-		puts "#{user.username} : p@ssw0rd"
-	  end
+	  	 user = User.by_username.key("#{username}#{surfix}".downcase).first
+
+		 if user.blank?
+
+			user = User.create(username: "#{username}#{surfix}", plain_password: "p@ssw0rd", last_password_date: Time.now,
+									password_attempt: 0, login_attempt: 0, first_name: users[username]["first_name"],
+									last_name: users[username]["last_name"], role: users[username]["role"],
+									email: "admin@baobabhealth.org",district_code: SETTINGS['district_code'])
+			puts "#{user.username} : p@ssw0rd"
+		 else
+		 	puts "User already exist"
+		 end
+		end
 elsif  SETTINGS['site_type'] == "remote"
 	District.all.each do |district|
 	  surfix = district.id
 	  puts "\nLogin credentials for #{district.name}"
 	  users.keys.each do |username|
-		user = User.create(username: "#{username}#{surfix}", plain_password: "p@ssw0rd", last_password_date: Time.now,
-								password_attempt: 0, login_attempt: 0, first_name: users[username]["first_name"],
-								last_name: users[username]["last_name"], role: users[username]["role"],
-								email: "admin@baobabhealth.org")
-		puts "#{user.username} : p@ssw0rd"
+	  	user = User.by_username.key("#{username}#{surfix}".downcase).first
+
+		if user.blank?
+			user = User.create(username: "#{username}#{surfix}".downcase , plain_password: "p@ssw0rd", last_password_date: Time.now,
+									password_attempt: 0, login_attempt: 0, first_name: users[username]["first_name"],
+									last_name: users[username]["last_name"], role: users[username]["role"],
+									email: "admin@baobabhealth.org", district_code: district.id)
+			puts "#{user.username} : p@ssw0rd"
+		else
+			puts "User already exist"
+		end
 	  end
 	end
 end
