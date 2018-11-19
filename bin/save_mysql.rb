47649e5ddffdcea2b438e79aeda202c6cb5b6f86
Emmanuel Mkambankhani <manzymkamba@gmail.com>
2018-11-19 17:26:28 +0200

    Save to MYSQL script include couch last sequence

--- bin/save_mysql.rb
+++ bin/save_mysql.rb
@@ -66,7 +66,21 @@ while page <= pages
 	page = page + 1
 end
 
-a = JSON.parse(RestClient.get("http://localhost:5984/edrs_dc/_changes"))
+couch_mysql_path =  "#{Rails.root}/config/couchdb.yml"
+db_settings = YAML.load_file(couch_mysql_path)
+
+couch_db_settings =  db_settings[Rails.env]
+
+couch_protocol = couch_db_settings["protocol"]
+couch_username = couch_db_settings["username"]
+couch_password = couch_db_settings["password"]
+couch_host = couch_db_settings["host"]
+couch_db = couch_db_settings["prefix"] + (couch_db_settings["suffix"] ? "_" + couch_db_settings["suffix"] : "" )
+couch_port = couch_db_settings["port"]
+
+changes_link = "#{couch_protocol}://#{couch_username}:#{couch_password}@#{couch_host}:#{couch_port}/#{couch_db}/_changes"
+
+a = JSON.parse(RestClient.get(changes_link))
 
 last_seq = CouchdbSequence.last
 last_seq = CouchdbSequence.new if last_seq.blank?
