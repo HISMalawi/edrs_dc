edf078073dcea61c316eb39abcde7d6ca67d2149
Emmanuel Mkambankhani <manzymkamba@gmail.com>
2018-11-20 10:38:13 +0200

    Approve record redone

--- app/controllers/dc_controller.rb
+++ app/controllers/dc_controller.rb
@@ -86,16 +86,11 @@ class DcController < ApplicationController
 
 
 	def approve_record
-		p1 = fork { 
-			person = Person.find(params[:id])
-			PersonRecordStatus.change_status(person, "MARKED APPROVAL")
-		    check_den_assignment
-			unlock_users_record(person)
-		}
-
-		Process.detach(p1)
-		
-		render :text => {marked: true}.to_json	
+	
+		person = Person.find(params[:id])
+		PersonRecordStatus.change_status(person, "MARKED APPROVAL")
+		unlock_users_record(person)
+		redirect_to "#{params[:next_url].to_s}"
 	end
 
 	def check_approval_status
