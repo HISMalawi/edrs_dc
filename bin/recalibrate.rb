b6b26c8d31c7aab2a5a7f75888111658158f23a4
Emmanuel Mkambankhani <manzymkamba@gmail.com>
2019-02-08 09:57:16 +0200

    Recalibrate refined

--- bin/recalibrate.rb
+++ bin/recalibrate.rb
@@ -1,14 +1,16 @@
 #Load metadata
+puts "#{Rails.root}"
+exit
 puts "########### LOADING METADATA PLEASE WAIT ITS GOING TO TAKE A MOMENT ###########"
-`cd #{Rails.root} && bundle exec rails r bin/script/load_metadata_to_sql.rb`
+`cd #{Rails.root} && bundle exec rails r bin/scripts/load_metadata_to_sql.rb`
 puts "########### DONE LOADING METADATA ###########"
 puts "########################################################################################"
 puts "########### LOADING DATA PLEASE WAIT ITS GOING TO TAKE A MOMENT ######################"
 #Save data to mysql
-`cd #{Rails.root} && bundle exec rails r bin/script/save_mysql.rb`
+`cd #{Rails.root} && bundle exec rails r bin/scripts/save_mysql.rb`
 puts "########################################################################################"
 puts "################################# DONE LOADING DATA #################################"
 puts "########################################################################################"
 puts "################################# ADDING ENTRIES TO CRONTAB #################################"
-puts `cd #{Rails.root} && bundle exec rails r bin/script/add_to_crontab.rb`
+puts `cd #{Rails.root} && bundle exec rails r bin/scripts/add_to_crontab.rb`
 puts "################################# DONE RECALIBRATION #################################"
