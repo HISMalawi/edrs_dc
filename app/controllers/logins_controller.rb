3db12ed308d8ab0dc890228d7cecde6b777f1ca2
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-06-03 07:50:14 +0200

    Changed login method

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -22,101 +22,17 @@ class LoginsController < ApplicationController
     #user = User.get_active_user(username)
     user = UserModel.where(username: username, active: 1).first
 
-    if SETTINGS['site_type'] == "remote"
-      district = DistrictRecord.where(name: params[:user][:district]).first
+    if user and user.password_matches?(password) and user.district_code == SETTINGS['district_code']
 
-      if user.present? && user.username != 'admin' && user.district_code != district.id
-              flash[:error] = 'User does not have rights for the district selected'
-              redirect_to "/login" and return
-      end
-    end
-    if user and user.password_matches?(password)
-      
       session[:current_user_id] = user.username
-      session[:expires_at] =  Time.current + 4.hours 
+      session[:expires_at] =  Time.current + 4.hours
 
-      ############## Checking if the user is from the district ####################################
-      if SETTINGS['site_type'] != "remote"
-
-          if (user.district_code != SETTINGS['district_code']) 
-            if (user.role !="System Administrator") && (user.site_code != "HQ")
-                logout!
-                flash[:error] = 'Your user credentilas is not from this district'
-                redirect_to "/", referrer_param => referrer_path and return
-            end
-            
-          end 
-      else
-        #do something if remote
-        if (user.role !="System Administrator") && (user.site_code != "HQ")
-          user_district = DistrictRecord.find(user.district_code).name
-
-          if SETTINGS['exclude'].split(",").include? user_district
-
-                logout!
-                flash[:error] = 'You user district has a pilot version'
-                redirect_to "/", referrer_param => referrer_path and return
-            
-          end
-        end
-
-      end
-
-      ###############################################################################################
-      
-      site_type = SETTINGS['site_type']
-      if site_type =="dc"
-        site_type = "DC"
-      else
-        site_type = site_type.humanize
-      end
-      roles = Role.by_level.key(site_type).collect{|r| r.role}
-      if roles.include? user.role
-        user_access = UserAccess.by_user_id.key(user.id).last
-        if SETTINGS['restrict_same_creatials_for_multiple_users'] && user_access.present?
-            flash[:error] = 'Another user has already login with the details you have entered'
-            redirect_to "/login" and return
-        end
-        
-        login!(user,params[:remote_portal])
-
-        # Password expirely should be refined
-=begin
-        if (Time.now.to_date - user.last_password_date.to_date).to_i >= 90 && false
-          
-           if user.role =="System Administrator"
-            redirect_to "/" and return 
-           end
-
-           if user.password_attempt >= 5 && (SETTINGS['password_can_expire'] rescue false)
-             logout!
-             flash[:error] = 'Your password has expired.Please contact your System Administrator.'
-             redirect_to "/", referrer_param => referrer_path and return
-           else
-             @password_attempt = user.password_attempt
-             user.update_attributes(:password_attempt => user.password_attempt + 1)
-
-             flash[:error] = 'Your password has expired.Please change it!!! or you will be logged out'
-             redirect_to "/change_password" and return
-           end
-        else
-          
-           if user.role !="System Administrator"
-             if (Time.now.to_date - user.last_password_date.to_date).to_i >= 85 && (Time.now.to_date - user.last_password_date.to_date).to_i < 90
-             		flash[:info] = 'Your password will expire soon. Please change it.'
-             end
-           end
-           redirect_to default_path and return
-        end 
-=end  
-        redirect_to default_path and return
-      else
-          flash[:error] = 'That username and/or password is not valid for this level'
-          redirect_to "/login" and return
-      end
+      login!(user,params[:remote_portal])
+      redirect_to default_path and return
+      #redirect_to "/" and return
     else
       flash[:error] = 'That username and/or password is not valid.'
-      redirect_to "/login" and return
+      redirect_to '/login' and return
     end
   end
 
