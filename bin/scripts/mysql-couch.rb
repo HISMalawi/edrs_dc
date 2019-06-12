7c1fdc84e7f08c421448d5f7f40b6d715e0a8e5f
Emmanuel Mkambankhani <manzymkamba@gmail.com>
2019-06-12 09:40:50 +0200

    Mysql-CouchDB

--- bin/scripts/mysql-couch.rb
+++ bin/scripts/mysql-couch.rb
@@ -15,6 +15,7 @@ couch_host = couch_db_settings["host"]
 couch_db = couch_db_settings["prefix"] + (couch_db_settings["suffix"] ? "_" + couch_db_settings["suffix"] : "" )
 couch_port = couch_db_settings["port"]
 
+puts "/////////////////////////////Records///////////////////////////////////////////////////"
 while page <= pages
 	Record.all.limit(200).offset(page * 200).each do |person|
 		#raise person.attributes.inspect
@@ -60,13 +61,155 @@ while page <= pages
 	  				puts  err.response.inspect
 				end				
 			end
-
+			puts "Missing Records #{json['type']} #{json['_id']}"
 		end
 	end
 
 	puts page
 	page = page + 1
 end
+
+
 puts "////////////////////////////////////////////////////////////////////////////////"
 puts "Missing Records #{id.count}"
 puts "////////////////////////////////////////////////////////////////////////////////"
+
+count = BarcodeRecord.count
+pagesize = 200
+pages = (count / pagesize) + 1
+
+page = 0
+
+db_settings = YAML.load_file("#{Rails.root}/config/couchdb.yml")
+couch_db_settings =  db_settings[Rails.env]
+
+couch_username = couch_db_settings["username"]
+couch_password = couch_db_settings["password"]
+couch_host = couch_db_settings["host"]
+couch_db = couch_db_settings["prefix"] + (couch_db_settings["suffix"] ? "_" + couch_db_settings["suffix"] : "" )
+couch_port = couch_db_settings["port"]
+puts "/////////////////////////////Barcode Records///////////////////////////////////////////////////"
+
+while page <= pages
+	BarcodeRecord.all.limit(200).offset(page * 200).each do |barcode|
+		couch_record = Barcode.find(barcode.id);
+		if couch_record.blank?
+			json = barcode.attributes
+			barcode.attributes.keys.each do |key|
+				json[key] = "" if json[key].blank?
+			end
+			json["_id"] = json["barcode_id"]
+			json.delete("barcode_id")
+			json.delete("_deleted")
+			json.delete("_rev")
+			json["type"] = "Barcode"
+
+			begin
+
+				RestClient.post("http://#{couch_username}:#{couch_password}@#{couch_host}:#{couch_port}/#{couch_db}", json.to_json, {content_type: :json, accept: :json})
+			rescue RestClient::ExceptionWithResponse => err
+	  			puts  err.response.inspect
+			end
+
+			puts "Missing Records #{json['type']} #{json['_id']}"
+		end
+
+	end
+	puts page
+	page = page + 1
+end
+
+
+
+count = RecordIdentifier.count
+pagesize = 200
+pages = (count / pagesize) + 1
+
+page = 0
+
+db_settings = YAML.load_file("#{Rails.root}/config/couchdb.yml")
+couch_db_settings =  db_settings[Rails.env]
+
+couch_username = couch_db_settings["username"]
+couch_password = couch_db_settings["password"]
+couch_host = couch_db_settings["host"]
+couch_db = couch_db_settings["prefix"] + (couch_db_settings["suffix"] ? "_" + couch_db_settings["suffix"] : "" )
+couch_port = couch_db_settings["port"]
+puts "/////////////////////////////Record Identifier///////////////////////////////////////////////////"
+
+while page <= pages
+	RecordIdentifier.all.limit(200).offset(page * 200).each do |identifier|
+		couch_record = PersonIdentifier.find(identifier.id);
+		if couch_record.blank?
+			json = identifier.attributes
+			identifier.attributes.keys.each do |key|
+				json[key] = "" if json[key].blank?
+			end
+			json["_id"] = json["person_identifier_id"]
+			json.delete("person_identifier_id")
+			json.delete("_deleted")
+			json.delete("_rev")
+			json["type"] = "PersonIdentifier"
+
+			begin
+
+				RestClient.post("http://#{couch_username}:#{couch_password}@#{couch_host}:#{couch_port}/#{couch_db}", json.to_json, {content_type: :json, accept: :json})
+			rescue RestClient::ExceptionWithResponse => err
+	  			puts  err.response.inspect
+			end
+
+			puts "Missing Records #{json['type']} #{json['_id']}"
+
+		end
+
+	end
+
+    puts page
+	page = page + 1
+end
+
+count = UserModel.count
+pagesize = 200
+pages = (count / pagesize) + 1
+
+page = 0
+
+db_settings = YAML.load_file("#{Rails.root}/config/couchdb.yml")
+couch_db_settings =  db_settings[Rails.env]
+
+couch_username = couch_db_settings["username"]
+couch_password = couch_db_settings["password"]
+couch_host = couch_db_settings["host"]
+couch_db = couch_db_settings["prefix"] + (couch_db_settings["suffix"] ? "_" + couch_db_settings["suffix"] : "" )
+couch_port = couch_db_settings["port"]
+puts "/////////////////////////////User Records///////////////////////////////////////////////////"
+
+while page <= pages
+	UserModel.all.limit(200).offset(page * 200).each do |user|
+		couch_record = User.find(user.id);
+		if couch_record.blank?
+			json = user.attributes
+			user.attributes.keys.each do |key|
+				json[key] = "" if json[key].blank?
+			end
+			json["_id"] = json["user_id"]
+			json.delete("user_id")
+			json.delete("_deleted")
+			json.delete("_rev")
+			json["type"] = "User"
+
+			begin
+
+				RestClient.post("http://#{couch_username}:#{couch_password}@#{couch_host}:#{couch_port}/#{couch_db}", json.to_json, {content_type: :json, accept: :json})
+			rescue RestClient::ExceptionWithResponse => err
+	  			puts  err.response.inspect
+			end
+
+			puts "Missing Records #{json['type']} #{json['_id']}"
+
+		end
+
+	end
+    puts page
+	page = page + 1
+end
